4af0a0221752773aa40efa46dd5d8b9e45b3a5dd
ashongwurei <ashongwurei@gmail.com>
2022-03-02 17:26:06 +0100

    Implemented the active link color

--- script.js
+++ script.js
@@ -3,6 +3,10 @@ const overlay = document.querySelector(".overlay");
 const btnOpen = document.querySelector(".open-nav-items");
 const btnClose = document.querySelector(".close-nav-items");
 const navLinks = document.querySelectorAll(".link--nav");
+const home = document.querySelector(".active");
+const about = document.getElementById("about-button");
+const projects = document.getElementById("projects-button");
+const contact = document.getElementById("contact-button");
 
 const openNavItems = function () {
   navItems.classList.remove("hidden");
@@ -24,3 +28,31 @@ overlay.addEventListener("click", closeNavItems);
 for (let i = 0; i < navLinks.length; i++) {
   navLinks[i].addEventListener("click", closeNavItems);
 }
+
+home.addEventListener("click", function () {
+  home.classList.add("active");
+  about.classList.remove("active");
+  projects.classList.remove("active");
+  contact.classList.remove("active");
+});
+
+about.addEventListener("click", function () {
+  about.classList.add("active");
+  home.classList.remove("active");
+  projects.classList.remove("active");
+  contact.classList.remove("active");
+});
+
+projects.addEventListener("click", function () {
+  projects.classList.add("active");
+  home.classList.remove("active");
+  about.classList.remove("active");
+  contact.classList.remove("active");
+});
+
+contact.addEventListener("click", function () {
+  contact.classList.add("active");
+  home.classList.remove("active");
+  about.classList.remove("active");
+  projects.classList.remove("active");
+});
